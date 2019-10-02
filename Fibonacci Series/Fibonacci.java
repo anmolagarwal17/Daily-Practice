bebc39809b5fb5bd8d98fd823f75859403d20266
Anmol-Agarwal <a.a.rocks17@gmail.com>
2019-10-03 03:26:10 +0530

    added exception handling and other stuff to take in consideration random inputs

--- Fibonacci Series/Fibonacci.java
+++ Fibonacci Series/Fibonacci.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class Fibonacci
 {
@@ -7,15 +8,33 @@ class Fibonacci
 		Scanner sca = new Scanner(System.in);
 		
 		long a=0, b=1, c=1, num;
-		System.out.println("Enter maximum number under which you want to get a Fibonacci series:");
-		num = sca.nextInt();
-		System.out.println(a);
-		while(c < num)
+		System.out.print("Enter maximum number under which you want to get a Fibonacci series:");
+		try
 		{
-			System.out.println(c);
-			c = a + b;
-			a = b;
-			b = c;
+			num = sca.nextInt();
+			
+			if(num < 0)
+			{
+				System.out.println("Invalid input.");
+				return;
+			}
+			
+			System.out.println(a);
+			
+			if(num == 0)		//only print zero then return
+				return;
+				
+			while(c <= num)
+			{
+				System.out.println(c);
+				c = a + b;
+				a = b;
+				b = c;
+			}
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Invalid input. \nException: "+e);
 		}
 	}
 }
