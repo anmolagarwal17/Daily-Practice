50d9420fb182d059b0fc6013662553cd4ab71ba3
Anmol-Agarwal <a.a.rocks17@gmail.com>
2019-10-04 23:14:21 +0530

    removed 0 and 1 to come as output as prime number

--- Prime Number/Prime.java
+++ Prime Number/Prime.java
@@ -9,6 +9,13 @@ class Prime
 		int num = sca.nextInt();
 		boolean prime = true;			// setting a number as prime by default
 
+		if(num<2)
+		{
+			System.out.println("The entered number is not Prime!");
+			return;
+		}
+
+
 		for(int i=2; i<=num/i; i++)		//for example if i=2 and num=16 then i<=num/2 ==> 2<=8  (if 16 is not divisible by 2 then it must not be divisible by any number greater than 'num/2' i.e 8)
 			if(num%i == 0)
 			{
--- Prime Number/PrimeInRange.java
+++ Prime Number/PrimeInRange.java
@@ -11,9 +11,18 @@ class PrimeInRange
 		System.out.print("Enter a ending number(inclusive):");
 		int end = sca.nextInt();
 
+		if(start>end)
+		{
+			System.out.println("Staring number cannot be greater than ending number!!!");
+			return;
+		}
 
 		for(int num=start; num <=end; num++)
 		{
+			if(num<2)
+			{
+				continue;
+			}
 			boolean prime = true;			// setting a number as prime by default
 			for(int i=2; i<=num/i; i++)		//for example if i=2 and num=16 then i<=num/2 ==> 2<=8  (if 16 is not divisible by 2 then it must not be divisible by any number greater than 'num/2' i.e 8)
 				if(num%i == 0)
